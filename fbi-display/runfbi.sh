432344b02d0b36bc545e329a4ed3f2a8114a6c17
Christopher Alessandro <calessandro0827@gmail.com>
2017-07-23 18:14:18 -0700

    Add script to run modprobe and fbi

--- fbi-display/runfbi.sh
+++ fbi-display/runfbi.sh
@@ -1,3 +1,6 @@
 #!/bin/sh -x
-echo "Running FBI"
+
+modprobe fbtft dma=1
+modprobe fbtft_device txbuflen=32768 busnum=32766 custom name=fb_ili9340 speed=36000000 gpios=reset:133,dc:134,led:34 bgr=1 rotate=90
+
 fbi --noverbose -a -t 5 -T 1 -d /dev/fb0 *.png
