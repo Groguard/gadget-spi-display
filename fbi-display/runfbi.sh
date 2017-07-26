7550cefcd1bcb707248fcbda26e8b2581c415de4
Christopher Alessandro <calessandro0827@gmail.com>
2017-07-26 10:01:00 -0700

    Added new container for loading fbtft removed txt commands

--- fbi-display/runfbi.sh
+++ fbi-display/runfbi.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -x
 
-modprobe fbtft dma=1
-modprobe fbtft_device txbuflen=32768 busnum=32766 custom name=fb_ili9340 speed=36000000 gpios=reset:133,dc:134,led:34 bgr=1 rotate=90
-
 fbi --noverbose -a -t 5 -T 1 -d /dev/fb0 *.png
+
+while [ ! -z "$(ps aux | grep fbi)" ]; do
+  echo "fbi is still running"
+  sleep 1s
+done
